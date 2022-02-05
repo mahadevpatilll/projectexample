866df26c2a43aee4420e66bc04eb08e88bde2b79
mahadev <mahadevpatil9035@gmail.com>
2022-02-05 11:11:26 +0530

    hello

---
+++ emaple/src/main/java/com/technoelevate/emaple/EmapleApplication.java
@@ -0,0 +1,13 @@
+package com.technoelevate.emaple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmapleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmapleApplication.class, args);
+	}
+
+}
---
+++ emaple/src/test/java/com/technoelevate/emaple/EmapleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technoelevate.emaple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmapleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
